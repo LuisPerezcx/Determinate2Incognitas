84885cf48c515ec78d3cb761e4622fe109a589a6
LuisPerezcx <luisperez90377@gmail.com>
2023-04-28 16:40:16 -0600

    con matriz

--- src/Determinante.java
+++ src/Determinante.java
@@ -1,29 +1,45 @@
 import java.util.Scanner;
 
 public class Determinante {
-    double a,b,c,d,e,f;
+    int filas =2;
+    int columnas =3;
+    double mattriz[][]=new double[filas][columnas];
     double valor1,valor2;
     public void calcular(Scanner scanner){
         ingresar(scanner);
-        valor1=(c*e)-(f*b);
-        valor2=(a*e)-(d*b);
+        System.out.println("Matiriz:");
+        mostrar();
+        System.out.println("\n Resultados:");
+        valor1=(mattriz[0][2]*mattriz[1][1])-(mattriz[1][2]*mattriz[0][1]);
+        valor2=(mattriz[0][0]*mattriz[1][1])-(mattriz[1][0]*mattriz[0][1]);
         System.out.println("X = " + (valor1/valor2));
-        valor1=(a*f)-(d*c);
-        valor2=(a*e)-(d*b);
+        valor1=(mattriz[0][0]*mattriz[1][2])-(mattriz[1][0]*mattriz[0][2]);
+        valor2=(mattriz[0][0]*mattriz[1][1])-(mattriz[1][0]*mattriz[0][1]);
         System.out.println("Y = " + (valor1/valor2));
     }
     private void ingresar(Scanner scanner){
-        System.out.println("ingresa x1: ");
-        a= scanner.nextDouble();
-        System.out.println("ingresa y1: ");
-        b= scanner.nextDouble();
-        System.out.println("ingresa el resultado de la ecuacion 1 ");
-        c= scanner.nextDouble();
-        System.out.println("ingresa x2: ");
-        d= scanner.nextDouble();
-        System.out.println("ingresa y2: ");
-        e= scanner.nextDouble();
-        System.out.println("ingresa el resultado de la ecuacion 2 ");
-        f= scanner.nextDouble();
+        int cont=0;
+        for(int i = 0; i< filas; i++){
+            cont=0;
+            for (int j = 0; j < columnas; j++) {
+                if(i==0){
+                    System.out.println("ingresa x"+(cont+1)+":");
+                    mattriz[i][j]= scanner.nextDouble();
+                }else{
+                    System.out.println("ingresa y"+(cont+1)+":");
+                    mattriz[i][j]= scanner.nextDouble();
+                }
+                cont++;
+            }
+            System.out.println();
+        }
+    }
+    private void mostrar(){
+        for(int i = 0; i< filas; i++){
+            for (int j = 0; j < columnas; j++) {
+                System.out.print(mattriz[i][j] + " | ");
+            }
+            System.out.println();
+        }
     }
 }
